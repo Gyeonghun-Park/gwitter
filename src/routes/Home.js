395d97cbc9f174ac8572695c1b92505d3f446041
Gyeonghun Park <gp112795@gmail.com>
2020-09-20 12:35:17 -0400

    Form and Databse Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../fbase";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [gWeet, setGweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("gweets").add({
+      gWeet,
+      createdAt: Date.now(),
+    });
+    setGweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setGweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={gWeet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Gweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
