e241fbd6ffc91c7f2dad7f71952c7e222dc59c21
Gyeonghun Park <gp112795@gmail.com>
2020-09-22 15:15:51 -0400

    Updates

---
+++ precache-manifest.8b817294f7bf93545b20729c546db0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4b9eacf0eaabc71bee686f41bd4ce7",
+    "url": "/gwitter/index.html"
+  },
+  {
+    "revision": "1086a15ff3303f7094a8",
+    "url": "/gwitter/static/css/main.500bd03b.chunk.css"
+  },
+  {
+    "revision": "da92a314142a7a3aaa62",
+    "url": "/gwitter/static/js/2.d41868c0.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/gwitter/static/js/2.d41868c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1086a15ff3303f7094a8",
+    "url": "/gwitter/static/js/main.11982278.chunk.js"
+  },
+  {
+    "revision": "ba1c4d7a2b4e15196290",
+    "url": "/gwitter/static/js/runtime-main.292f2647.js"
+  }
+]);
